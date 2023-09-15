a70144547dc054dda8e44ec5c7f1ce04bc0e4aea
MyratJr <begmyradowmyratgeldi@gmail.com>
2023-09-15 12:48:30 +0500

    salam

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean
 from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
