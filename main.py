549665b4b4d169a2bf23bb592f0c7121ca234a18
unknown <begmyradowmyratgeldi.com>
2023-09-21 11:20:47 +0500

    hello

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from auth.schemas import UserRead, UserCreate
 from operations.router import router as router_operations
 from tasks.router import router
 from fastapi.middleware.cors import CORSMiddleware
+from pages.router import router as router_pages
+from fastapi.staticfiles import StaticFiles
 
 app=FastAPI(title="Trading app")
 
+app.mount('/static',StaticFiles(directory="static"),name="static")
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
@@ -22,18 +25,24 @@ app.include_router(
 
 app.include_router(router_operations)
 app.include_router(router)
+app.include_router(router_pages)
 
 
-# origins = [
-#     "http://localhost:3000",
-#     "https://amazon.com"
-# ]
+origins = [
+    "http://localhost:3000",
+    "https://amazon.com"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET","POST","OPTIONS","DELETE","PATCH","PUT"],
+    allow_headers=["Content_Type","Set-Cookie","Access-Control-Allow-Headers","Authorization"]
+)
 
 
 
 @app.get("/")
 async def main():
     return {"message": "Hello World"}
-
-
